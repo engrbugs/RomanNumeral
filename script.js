c9cc72ef542354b0496bb890c4b053b1236f7b1d
engrbugs <engrbugs@gmail.com>
2020-06-20 17:23:46 -0700

    done

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('hw');
+const RNtable = {
+    'M': 1000,
+    'CM': 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
+}
+
+
+function convertToRoman(num) {
+    let roman = '';
+    for (let i in RNtable) {
+        while (num >= RNtable[i]) {
+            num -= RNtable[i];
+            roman += i;
+        }
+    }
+    return roman;
+}
+
+console.log(convertToRoman(891));
